7b65eb4299016940bab45bcf425a8a3f4b7d530b
Lasse27 <120169365+Lasse27@users.noreply.github.com>
2025-11-19 21:53:06 +0100

    Update .gitignore to exclude static directory; modify deploy script for clearer static folder management

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,21 @@
+#--------------------------------
 # Vue-Projekt bauen
-# "--prefix HomeStorage.UI" sagt npm, wo das Package.json liegt
+#--------------------------------
 npm run build --prefix HomeStorage.UI
+$staticP = "HomeStorage.Server\app"
+$staticF = "HomeStorage.Server\app\static"
 
-# Alten Flask Static-Ordner leeren
-# -Recurse = alle Unterordner einbeziehen
-# -Force = auch schreibgeschützte Dateien löschen
-Remove-Item -Path "HomeStorage.Server\app\static\*" -Recurse -Force
+# Alten Flask Static-Ordner löschen
+if ((Test-Path $staticF)) {
+    Remove-Item -Path $staticF -Recurse -Force
+}
+
+# Ordner neu erstellen
+New-Item $staticP -Name "static" -ItemType Directory
 
 #  Vue Build in Flask static kopieren
-# -Recurse = alle Unterordner und Dateien kopieren
-# -Force = bestehende Dateien überschreiben
 Copy-Item -Path "HomeStorage.UI\dist\*" -Destination "HomeStorage.Server\app\static" -Recurse -Force
 
 # Status ausgeben
 Write-Host "Vue Build nach Flask static/ kopiert"
+
