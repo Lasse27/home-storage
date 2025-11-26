a7179287222216e5f5132feae1fcba52474afe91
Lasse27 <120169365+Lasse27@users.noreply.github.com>
2025-11-26 22:08:42 +0100

    Remove QuickActions component from the dashboard and clean up related styles

--- HomeStorage.UI/src/pages/DashBoard.vue
+++ HomeStorage.UI/src/pages/DashBoard.vue
@@ -13,7 +13,6 @@ import { Home } from 'lucide-vue-next'
         <div class="panel-container">
             <!-- Dashboard content goes here -->
             <RecentlyAdded style="grid-area: recently_added;" />
-            <QuickActions style="grid-area: quick_actions;" />
             <SystemSummary style="grid-area: system;" />
         </div>
     </div>
@@ -32,30 +31,10 @@ import { Home } from 'lucide-vue-next'
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-template-areas:
-        "recently_added system"
-        "quick_actions system";
-    grid-template-rows: 1fr 1fr;
+        "recently_added system";
     height: 100%;
-    gap: 1.5rem;
-    padding: 1.5rem;
+    gap: 1rem;
+    padding: 1rem;
     box-sizing: border-box;
-}
-
-.home-page .panel {
-    background-color: var(--clr-surface-a10);
-    border: 1px solid var(--clr-surface-a20);
-    border-radius: 1rem;
-    box-sizing: border-box;
-}
-
-.panel .panel-header {
-    box-sizing: border-box;
-    padding: 0.75rem;
-    font-size: 1.25rem;
-    width: 100%;
-    align-items: center;
-    text-align: center;
-    background-color: var(--clr-surface-a30);
-    border-radius: 1rem 1rem 0 0;
 }
 </style>
